94f41b513d898ea20ccaeb46f2984117362f195d
Nidhi Hemanth <59618904+NidhiHemanth@users.noreply.github.com>
2022-01-13 21:21:25 +0530

    Supervisor class implementation

--- Supervisor.java
+++ Supervisor.java
@@ -1,38 +1,50 @@
 package StudyFund;
 
-//
+import Project;
+
+/* 
+    CHECK PROJECT STATUS
+  
+    Input & Output
+        input  : project, department (optional)
+        output : comments 
+    Normal scenario
+        case 1 : main supervisor
+            checks progress of all department supervisors
+        case 2 : department supervisors
+            checks progress of the function of the particular department
+        
+        returns the project status
+*/
+
 public class Supervisor extends Implementor {
-
-    // Default Constructor
-    public Supervisor() {
-    }
-
-    //
     public String department;
 
-    //
-    public void check_project_status() {
-        // TODO implement here
+    public Supervisor() {
+        // Default Constructor
+    }
+    
+    // Main supervisor
+    public String check_project_status(int pid) {
+        if(stringCompare(pid.getProjectStatus(), "Done") == 0)
+            return "Good Job!";
+        else
+            return "We can do this project guys!";
     }
 
-    //
     public void manage_budget() {
         // TODO implement here
     }
 
-    //
     public void provide_status() {
         // TODO implement here
     }
 
-    //
     public void manages_workflow() {
         // TODO implement here
     }
 
-    //
     public void submit_results() {
         // TODO implement here
     }
-
 }
